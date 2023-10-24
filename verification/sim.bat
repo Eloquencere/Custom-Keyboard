90082fd7d676cc8a3437e2db08c99a6fad1611b9
Srirangarajan™ <106532953+Eloquencere@users.noreply.github.com>
2023-10-24 22:23:03 +0530

    Update sim.bat

--- verification/sim.bat
+++ verification/sim.bat
@@ -1,6 +1,6 @@
 cd sim_data/
 cls
 vlog -f "../Compile_Order_Filelist.f"
-vsim TestBench_Top -sv_seed random -c -do "run -all;exit" -solvefaildebug=2
+vsim TestBench_Top -sv_seed random -c -do "run -all;exit"
 @echo off
 cd ..
